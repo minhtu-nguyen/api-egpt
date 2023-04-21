76bc5aa197b8b0ca07ac5f7bfc25da9a2c6e5b0e
minhtu-nguyen <minhtu-nguyen@outlook.com>
2023-04-21 14:17:52 +0700

    First commit

--- app/main.py
+++ app/main.py
@@ -119,7 +119,3 @@ async def post_audio(file: UploadFile = File(...)):
     # Use for Post: Return output audio
     return StreamingResponse(iterfile(), media_type="application/octet-stream")
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, ssl_keyfile="./localhost+2-key.pem", 
-                ssl_certfile="./localhost+2.pem"
-                )
